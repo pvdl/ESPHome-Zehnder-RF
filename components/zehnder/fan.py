0dfa96693787305a619ef6120f0db390c89a85a8
Peter <vanderlaan.pm@gmail.com>
2026-08-09 21:44:18 +0200

    Refactor preset modes validation in fan component

--- components/zehnder/fan.py
+++ components/zehnder/fan.py
@@ -1,7 +1,8 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import fan
-from esphome.const import CONF_ID, CONF_UPDATE_INTERVAL
+from esphome.components.fan import validate_preset_modes
+from esphome.const import CONF_ID, CONF_PRESET_MODES, CONF_UPDATE_INTERVAL
 
 from esphome.components.nrf905 import nRF905Component
 from . import zehnder_ns, ZehnderRF
@@ -10,13 +11,12 @@ from . import zehnder_ns, ZehnderRF
 DEPENDENCIES = ["nrf905"]
 
 CONF_NRF905 = "nrf905"
-CONF_PRESET_MODES = "preset_modes"
 
 CONFIG_SCHEMA = fan.fan_schema(ZehnderRF).extend(
     {
         cv.Required(CONF_NRF905): cv.use_id(nRF905Component),
         cv.Optional(CONF_UPDATE_INTERVAL, default="30s"): cv.update_interval,
-        cv.Optional(CONF_PRESET_MODES): cv.ensure_list(cv.string_strict),
+        cv.Optional(CONF_PRESET_MODES): validate_preset_modes,
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
