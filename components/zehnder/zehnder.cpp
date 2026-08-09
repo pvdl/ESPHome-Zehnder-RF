92c35ac0e627f25e32354c7c3884a211880ba199
Peter <vanderlaan.pm@gmail.com>
2026-08-09 21:06:55 +0200

    Add supported preset modes to ZehnderRF setup
    Added supported preset modes for ZehnderRF setup.

--- components/zehnder/zehnder.cpp
+++ components/zehnder/zehnder.cpp
@@ -115,6 +115,14 @@ void ZehnderRF::control(const fan::FanCall &call) {
 }
 
 void ZehnderRF::setup() {
+  this->set_supported_preset_modes({
+      "Auto",
+      "Low",
+      "Medium",
+      "High",
+      "Max"
+  });
+  
   ESP_LOGCONFIG(TAG, "ZEHNDER '%s':", this->get_name().c_str());
 
   // Clear config
