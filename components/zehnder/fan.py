b82150609503b8fc318ead4bbf979e0df21e1439
Peter <vanderlaan.pm@gmail.com>
2026-08-09 21:11:30 +0200

    Add support for preset modes in fan configuration

--- components/zehnder/fan.py
+++ components/zehnder/fan.py
@@ -10,11 +10,13 @@ from . import zehnder_ns, ZehnderRF
 DEPENDENCIES = ["nrf905"]
 
 CONF_NRF905 = "nrf905"
+CONF_PRESET_MODES = "preset_modes"
 
 CONFIG_SCHEMA = fan.fan_schema(ZehnderRF).extend(
     {
         cv.Required(CONF_NRF905): cv.use_id(nRF905Component),
         cv.Optional(CONF_UPDATE_INTERVAL, default="30s"): cv.update_interval,
+        cv.Optional(CONF_PRESET_MODES): cv.ensure_list(cv.string_strict),
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
@@ -26,5 +28,7 @@ async def to_code(config):
 
     nrf905 = await cg.get_variable(config[CONF_NRF905])
     cg.add(var.set_rf(nrf905))
-
     cg.add(var.set_update_interval(config[CONF_UPDATE_INTERVAL]))
+
+    if CONF_PRESET_MODES in config:
+        cg.add(var.set_supported_preset_modes(config[CONF_PRESET_MODES]))
